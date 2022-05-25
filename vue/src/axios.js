69af12002bd7c62f046352a947dfad40aaf600e4
facundoagueria1@gmail.com <facundoagueria1@gmail.com>
2022-05-25 14:05:51 -0300

    Change baseURL API

--- vue/src/axios.js
+++ vue/src/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import store from "./store";
 
 const axiosClient = axios.create({
-  baseURL: `${import.meta.env.BASE_URL}/api`,
+  baseURL: `${import.meta.env.VITE_API_BASE_URL}/api`,
 });
 
 axiosClient.interceptors.request.use((config) => {
