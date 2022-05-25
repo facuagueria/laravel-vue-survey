312b036fc6eff4c465d8017c3df5ac0a64f8a7bf
facundoagueria1@gmail.com <facundoagueria1@gmail.com>
2022-05-25 14:46:02 -0300

    Fix paginate

--- app/Http/Controllers/SurveyController.php
+++ app/Http/Controllers/SurveyController.php
@@ -22,7 +22,7 @@ class SurveyController extends Controller
     public function index(Request $request)
     {
         $user = $request->user();
-        return SurveyResource::collection(Survey::where('user_id', $user->id)->paginate(1));
+        return SurveyResource::collection(Survey::where('user_id', $user->id)->paginate(10));
     }
 
     public function store(StoreSurveyRequest $request)
